81b156397f0633251fbb2673a221f537fd7e4b96
Angelos <ssolomon184@gmail.com>
2022-10-24 17:03:34 +0300

    replace codegen tests

--- test/codegeneration/neg_arr_alloc.java
+++
@@ -1,8 +0,0 @@
-class Main {
-  public static void main(String[] a) {
-    boolean[] b;
-    int x;
-    x = 1 - 2;
-    b = new boolean[x];
-  }
-}
--- test/codegeneration/neg_arr_alloc2.java
+++
@@ -1,8 +0,0 @@
-class Main {
-  public static void main(String[] a) {
-    int[] b;
-    int x;
-    x = 1 - 2;
-    b = new int[x];
-  }
-}
